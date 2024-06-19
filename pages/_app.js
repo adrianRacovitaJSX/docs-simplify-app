5f6412be0b5c7c855b2240020d7700c4762f59bf
adrianRacovitaJSX <aracovita@iempresa.net>
2024-06-19 19:27:12 +0200

    added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import '../globals.css'
- 
+import "../globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GoogleAnalytics gaId="G-BZ5MT3FJ0B" />
+      <Component {...pageProps} />
+    </>
+  );
 }
